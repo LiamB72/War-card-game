04dce638263ed1cf157dd4f2ff8e15936e8f9e3e
Liam B. <112952569+LiamB72@users.noreply.github.com>
2024-05-12 15:51:28 +0200

    Pushing out the whole game
    The whole thing is, in fact, made by me. The concept of the game itself is not. However the application and functions and stuff are.

---
+++ scripts/utils.py
@@ -0,0 +1,10 @@
+from PyQt6.QtWidgets import QMessageBox
+from PyQt6.QtGui import QIcon
+
+def warningBox(windowTitle: str, message: str, path: str):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Icon.Warning)
+    msg.setWindowIcon(QIcon(path))
+    msg.setText(message)
+    msg.setWindowTitle(windowTitle)
+    msg.exec()
